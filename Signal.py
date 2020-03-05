fixed bad scale/translate in Square, Triangle and Sawtooth. Average_samples works with *args richer visualisation of filter response

Signal.py
@@ -311,7 +311,10 @@ class Triangle(Signal):
     
     def generate(self, sample_rate):
         # strange manipulation on sawtooth
-        return 2*np.abs((2*np.pi* self.frequency * np.linspace(0, self.duration/1000, self.samples(sample_rate), False) % (2*np.pi))-np.pi)-np.pi
+        return 2*np.abs((2*np.pi
+                       * self.frequency
+                       * np.linspace(0, self.duration/1000, self.samples(sample_rate), False)
+                       % (2*np.pi) - np.pi))/np.pi - 1
     
     
 class Square(Signal):
@@ -321,7 +324,14 @@ class Square(Signal):
         self.duration = duration
     
     def generate(self, sample_rate):
-        return (((2*np.pi* self.frequency * np.linspace(0, self.duration/1000, self.samples(sample_rate), False) % (2*np.pi)) < np.pi) - np.pi).astype(np.float64)
+        return (((  2*np.pi
+                  * self.frequency
+                  * np.linspace(0,
+                                self.duration/1000,
+                                self.samples(sample_rate),
+                                False)
+                  % (2*np.pi)
+                  < np.pi)*2 - 1).astype(np.float64))
 
 class Sawtooth(Signal):
     def __init__(self, frequency=220, duration=5000):
@@ -330,7 +340,7 @@ class Sawtooth(Signal):
         self.duration = duration
     
     def generate(self, sample_rate):
-        return (2*np.pi* self.frequency * np.linspace(0, self.duration/1000, self.samples(sample_rate), False) % (2*np.pi))-np.pi
+        return (2*np.pi* self.frequency * np.linspace(0, self.duration/1000, self.samples(sample_rate), False) % (2*np.pi))/np.pi-1
 
 class Step(Signal):
     def __init__(self, duration=1):

tests/testsOld.py
@@ -118,12 +118,12 @@ def downsample_test(filename):
 
 def averagesample_test(filename):
     wav = WAV(filename)
-    #wav *= Average_samples(weights=(5,4,3,2,1,2,3,4,5))
-    wav *= Average_samples(weights=(1,1,1,1,1,1,1,1,1))
-    #wav *= Average_samples(weights=(25,16,9,4,1,4,9,16,25))
-    #wav *= Average_samples(weights=(1,0,0,0,0,0,0,0,1))
-    #wav *= Average_samples(weights=(1,-1,1,-1,1,-1,1,-1,1))
-    #wav *= Average_samples(weights=(-1,-1,-1,-1,10,-1,-1,-1,-1)) # high pass!
+    #wav *= Average_samples(5,4,3,2,1,2,3,4,5)
+    wav *= Average_samples(1,1,1,1,1,1,1,1,1)
+    #wav *= Average_samples(25,16,9,4,1,4,9,16,25)
+    #wav *= Average_samples(1,0,0,0,0,0,0,0,1)
+    #wav *= Average_samples(1,-1,1,-1,1,-1,1,-1,1)
+    #wav *= Average_samples(-1,-1,-1,-1,10,-1,-1,-1,-1) # high pass!
     audio = wav.mixdown(sample_rate=44100, byte_width=2)
     
     play_Audio(audio, is_wait=True)
@@ -134,10 +134,10 @@ def dummy_reverb_test():
     #amp = lambda x: 
     #wav = WAV(filename) + WAV(filename)*Amplitude(amp)*Shift(duration=500)
     #wav = WAV(filename)*AmpFreq(frequency=0.12, size=0.25)
-    #wav += WAV(filename)*AmpFreq(frequency=0.12, size=0.25, phase=np.pi)*Shift(duration=500)*Average_samples(weights=(1,1,1,1,1,1,1,1,1))
+    #wav += WAV(filename)*AmpFreq(frequency=0.12, size=0.25, phase=np.pi)*Shift(duration=500)*Average_samples(1,1,1,1,1,1,1,1,1)
     
-    wav = sum([(1-8/10)*WAV(filename)*Shift(duration=100*x)*Average_samples(weights=2*x+1) for x in range(5)])
-    wav += 0.6*WAV(filename)*Downsample_rough(factor=5)*Average_samples(weights=5)
+    wav = sum([(1-8/10)*WAV(filename)*Shift(duration=100*x)*Average_samples(2*x+1) for x in range(5)])
+    wav += 0.6*WAV(filename)*Downsample_rough(factor=5)*Average_samples(5)
     
     audio = wav.mixdown(sample_rate=44100, byte_width=2)
     play_Audio(audio, is_wait=True)
@@ -146,16 +146,16 @@ def dummy_reverb_test():
 def repan_reverb_test():
     filename = "data/african_sketches_1.wav"
     
-    wav = sum([(1-8/10)*WAV(filename)*Shift(duration=100*x)*Average_samples(weights=2*x+1)*Pan((1,0.3)[::(1 if x%2==0 else -1)]) for x in range(5)])
-    wav += 0.6*WAV(filename)*Pan((0,None))*Downsample_rough(factor=5)*Average_samples(weights=5)
+    wav = sum([(1-8/10)*WAV(filename)*Shift(duration=100*x)*Average_samples(2*x+1)*Pan((1,0.3)[::(1 if x%2==0 else -1)]) for x in range(5)])
+    wav += 0.6*WAV(filename)*Pan((0,None))*Downsample_rough(factor=5)*Average_samples(5)
     wav += 0.6*WAV(filename)*Pan((None,1))
     
     audio = wav.mixdown(sample_rate=44100, byte_width=2)
     play_Audio(audio, is_wait=True)
 
 def reverse_test():
-    wav = WAV("data/african_sketches_1.wav")*Reverse()*Downsample_rough(factor=5)*Average_samples(weights=5)
-    wav += WAV("data/african_sketches_1.wav")*Shift(duration=150)*Average_samples(weights=(1,1,1,1,1,1,1,1,1))
+    wav = WAV("data/african_sketches_1.wav")*Reverse()*Downsample_rough(factor=5)*Average_samples(5)
+    wav += WAV("data/african_sketches_1.wav")*Shift(duration=150)*Average_samples(1,1,1,1,1,1,1,1,1)
     
     audio = wav.mixdown(sample_rate=44100, byte_width=2, max_amplitude=0.09)
     play_Audio(audio, is_wait=True)

transforms.py
@@ -342,7 +342,7 @@ class Average_samples(Transform):
     of downsample rough.
     """
     
-    def __init__(self, weights):
+    def __init__(self, *weights):
         """
         weights is int -> average #weights neighboring samples
         weights is tuple -> average len(weights) neighboring samples,
@@ -350,11 +350,11 @@ class Average_samples(Transform):
         TODO: on the edges this causes a small fade in fade out of length len(weights).
         not necessarily a bug though.
         """
-        assert isinstance(weights, (int, tuple, list)), "weights argument should be either int or list/tuple"
-        #assert isinstance(weights, int) or sum(weights) == 1, "weights must sum to 1"
-        if isinstance(weights, int):
-            weights = tuple(1 for w in range(weights))
+        if len(weights) == 1:
+            weights = tuple(1 for w in range(weights[0]))
         
+        total = sum(weights)
+        weights = [w/total for w in weights]
         self.weights = weights
     
     def realise(self, audio):
@@ -362,8 +362,6 @@ class Average_samples(Transform):
         
         for i, weight in enumerate(self.weights):
             res[:,i:audio.audio.shape[1]+i] += weight*audio.audio
-        
-        res /= sum(self.weights)
         
         pos = int(np.floor((len(self.weights) - 1)/2))
         # the index from which to start reading the averaged signal