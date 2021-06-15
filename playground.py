5afde81b10ae164e7cd7aa056327e8e045ae4465
Ziv Kaplan <zivkaplan@gmail.com>
2021-06-15 12:23:53 +0300

    fixes v1 - added Fade for backwards compatibility

--- playground.py
+++
@@ -1,8 +0,0 @@
-from gensound import WAV, test_wav, Fade, FadeIn, FadeOut, Sine
-
-wav = Sine() # load sample WAV, included with gensound
-
-wav *= FadeIn()
-# wav *= FadeOut(curve="polynomial", duration=4e3)
-# wav *= Fade()
-wav.play()
