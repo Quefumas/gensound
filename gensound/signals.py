1578f560cc95c786f4d1518c30ca3b200dff4041
Quefumas <Quefumas>
2022-11-27 00:07:29 +0200

    cleaned parts of signal.py and added some docstrings

--- gensound/signals.py
+++ gensound/signals.py
@@ -1,264 +1,319 @@
-# -*- coding: utf-8 -*-
+"""
+Defines the Signal class and all its subclasses, which generate streams of audio
+and perform operations on them.
+"""
 
 import copy
+from collections.abc import Iterable
 
 import numpy as np
 
 from gensound.utils import isnumber, iscallable, num_samples
 from gensound.musicTheory import parse_melody_to_signal, read_freq
-
 from gensound.transforms import Transform, TransformChain, Amplitude, Slice, Combine, BiTransform
 from gensound.curve import Curve
 from gensound.audio import Audio
-
-#CHANNEL_NAMES = {"L":0, "R":1}
-
 
 __all__ = ["Signal", "Silence", "Step", "WhiteNoise", "Sine", "Triangle",
            "Square", "Sawtooth", "Raw", "WAV"]
 
 class Signal:
+    """
+    A Signal object is an abstract recipe to generate a stream of audio that may be played or exported.
+    Signals can be combined with each other in various ways, and have Transforms applied to them,
+    which are recipes to modify the audio stream after the fact.
+
+    Signal objects behave like immutable types when performing most operations.
+    """
+
     def __init__(self):
         self.transforms = []
-    
-    #@abstractmethod ???
+
     def generate(self, sample_rate):
         """
-        this is the part the generates the basic signal building block
-        ####should return 2d np.ndarray
-        should return 1d np.ndarray, the dims are fixed later...
-        not sure if good
-        TODO
-        new: can also return Audio if there is a need;
-        but generally should be np.ndarray
+        Override this to implement the actual creation of the underlying audio stream
+        to be further processed by the Transforms applied to this Signal.
+
+        This should return a 2d numpy.ndarray of floats, but returning an Audio object
+        is acceptable as well.
         """
-        return np.zeros(shape=(1,0))
-    
+        return np.zeros(shape=(1, 0))
+
     def realise(self, sample_rate):
-        """ returns Audio instance.
-        parses the entire signal tree recursively
+        """
+        Applies transforms and other operations to the base audio stream created by generate().
+        Returns an Audio object.
         """
         audio = self.generate(sample_rate)
-        
+
         if not isinstance(audio, Audio):
             audio = Audio(sample_rate).from_array(audio)
-    
+
         for transform in self.transforms:
             transform.realise(audio=audio)
-            
+
         return audio
-    
+
     mixdown = realise # TODO consider keeping just 1
-    
+
+    #### I/O operations
     def play(self, sample_rate=44100, **kwargs):
+        """
+        Realises the signal and plays the resulting audio.
+        """
         audio = self.realise(sample_rate)
         return audio.play(**kwargs)
-    
+
     def export(self, filename, sample_rate=44100, **kwargs):
+        """
+        Realises the signal and exports the resulting audio to file.
+        """
         audio = self.realise(sample_rate)
         audio.export(filename, **kwargs)
-    
+
     def to_bytes(self, sample_rate=44100, **kwargs):
+        """
+        Realises the signal and returns a byte stream containing the audio as it would be represented in memory.
+        This is useful when retrieving raw audio for use with other modules or I/O.
+        """
         audio = self.realise(sample_rate)
         audio._prepare_buffer(**kwargs)
         return audio.buffer
-    
+
     #####################
     def num_samples(self, sample_rate):
+        """
+        Computes the length of the signal in samples (int).
+        """
+        # TODO: not all Signal implementations define 'duration'. Should this function exist only for certain subclasses?
+        # Or should all Signals define duration?
         return num_samples(self.duration, sample_rate)
-    
+
     def sample_times(self, sample_rate):
+        """
+        Returns a numpy.ndarray with the sample times (in secs) for this signal, relative to signal start.
+        """
+        # TODO: not all subclasses define self.duration. Should only certain subclasses have this? (Osc?)
         stop_time = self.duration/1000 if isinstance(self.duration, float) \
             else self.duration/sample_rate
+
         return np.linspace(start=0, stop=stop_time, num=self.num_samples(sample_rate), endpoint=False)
-    
-        
+
     def copy(self):
         """
-        creates an identical signal object.
+        Returns an identical Signal object.
         """
-        # TODO perhaps deepcopy should be overridden to be thinner and faster,
-        # since it is performed often
+        # TODO validate this implementation, to ensure the recursion does copy everything
+        # TODO OTOH consider whether this implementation may be wasteful, since this is performed often.
         return copy.deepcopy(self)
-    
+
     @staticmethod
     def concat(*args):
         """
+        Returns a Signal object (Sequence) concatenating the argument Signals (i.e., playing them one by one).
+
         If receives a single Signal, return it unchanged.
-        If receives a list/tuple of Signals, return a Sequence object containing them
-        If receives multiple arguments, return a Sequence containing them
+        If receives an iterable of Signals, return a Sequence object containing them.
+        If receives multiple Signal arguments, return a Sequence containing them.
         """
         if len(args) == 1:
             if isinstance(args[0], Signal):
                 return args[0]
-            else: # assumes tuple/list/iterable
+            elif isinstance(args[0], Iterable):
                 return Sequence(*args[0])
+            else:
+                return NotImplemented
         return Sequence(*args)
-    
+
     @staticmethod
-    def mix(args):
-        # we really want to use #reduce(, args) instead
-        # but the question is what should be the 1st argument?
-        return sum(args)
-    
-    ####### sound operations ########
-    
+    def mix(*args):
+        """
+        Returns a Signal object (Mix) mixing the argument Signals (playing them simultaneously).
+
+        If receives a single Signal, return it unchanged.
+        If receives an iterable of Signals, return a Mix object containing them.
+        If receives multiple Signal arguments, return a Mix containing them.
+        """
+        # TODO test this
+        # TODO should the args be copied before mixing?
+        if len(args) == 1:
+            if isinstance(args[0], Signal):
+                return args[0]
+            elif isinstance(args[0], Iterable):
+                return Mix(*args[0])
+        return Mix(*args)
+
+    #### audio operations
     def _amplitude(self, number):
+        """
+        Returns an identical Signal, with amplitude multiplied by the given number.
+        """
         assert isnumber(number)
-        return self*Amplitude(number)
-    
+        return self._apply(Amplitude(number))
+
     def _apply(self, transform):
+        """
+        Returns an identical Signal which applies the given Transform to the audio stream after generation.
+
+        If the transform is a number, it is interpreted as applying the Amplitude transform.
+        """
         assert not isinstance(transform, BiTransform), "can't apply BiTransform, use concat."
-        
+
         if transform is None:
             return self
-        
+
         if isnumber(transform):
             return self._amplitude(transform)
-        
+
         if iscallable(transform) and not isinstance(transform, Transform):
-            # if transform is a function receiving a signal and returning a signal,
-            # masquerading as a Transform. Use @transform decorator for those
+            # transform is a function decorated with @transform, receiving and returning a signal.
             return transform(self)
-        
-        s = self.copy() # TODO is this needed?
-        # it is, so we can reuse the same base signal multiple times
+
+        signal = self.copy()  # TODO ensure this is needed and working properly
+
         if isinstance(transform, TransformChain):
-            s.transforms.extend(transform.transforms)
+            signal.transforms.extend(transform.transforms)
         else:
-            s.transforms.append(transform)
-        return s
-    
+            signal.transforms.append(transform)
+        return signal
+
     def _concat(self, other):
-        # TODO why is this before mix?
-        # TODO consider enabling for negative other,
-        # thus shifting a sequence forward(backward? Chinese) in time
-        # TODO possibly better way to implement than using Silence
+        """
+        Returns a new Signal, containing this Signal concatenated to the other Signal.
+
+        The other signal may also be a positive number, which is interpreted as adding silence of the given duration,
+        or a BiTransform.
+        """
         if other is None:
             return self
-        
+
         if isnumber(other):
+            # TODO consider enabling for negative number other, which could add a tail perhaps
+            # TODO consider implementing directly, without creating a new object for this purpose
             other = Silence(duration=other)
-            
-        s = Sequence()
-        # use isinstance(self, Sequence) instead? more semantic
-        # TODO write this as overloading of Sequence operators instead?
+
+        signal = Sequence()
+        # TODO maybe overload Sequence.__add__ instead?
         if not self.transforms and isinstance(self, Sequence):
-            s.sequence += self.sequence
+            signal.sequence += self.sequence  # TODO shouldn't we copy each of these?
         else:
-            s.sequence += [self]
-        
-        if isinstance(other, BiTransform): # if concatting BiTransform
-            s.sequence[-1] = s.sequence[-1]._apply(other.L)
-            s.sequence += [other.R]
-            return s
-        
-        if isinstance(s.sequence[-1], Transform): # if coming out of BiTransform
-            t = s.sequence.pop()
+            signal.sequence += [self]  # TODO likewise
+
+        # concatting a BiTransform
+        if isinstance(other, BiTransform):
+            # TODO shouldn't the transforms be copied as well?
+            signal.sequence[-1] = signal.sequence[-1]._apply(other.L)
+            # put aside the second part of the BiTransform, to be applied on the next signal to be concatted
+            signal.sequence += [other.R]
+            return signal
+
+        # concatting a signal after a BiTransform
+        if isinstance(signal.sequence[-1], Transform):
+            t = signal.sequence.pop()
         else:
             t = None
-        
+
         if not other.transforms and isinstance(other, Sequence):
             other.sequence[0] = other.sequence[0]._apply(t)
-            s.sequence += other.sequence
+            signal.sequence += other.sequence
         else:
-            s.sequence += [other._apply(t)]
-        
-        return s
-    
+            signal.sequence += [other._apply(t)]
+
+        return signal
+
     def _mix(self, other):
-        # mixing with constant number is interpreted as DC
+        """
+        Return a new Signal (Mix), containing this Signal mixed with another Signal (played simultaneously).
+
+        If the other operand is a number, this adds DC to the original Signal. This increases all samples by the given
+        amount, which is typically not audible, and may cause damage to equipment.
+        """
         # TODO use @singledispatchmethod for this?
         if isnumber(other):
-            # TODO not all Signals define self.duration!
-            # This looks like a case for duration inference
-            # luckily this isn't actually a very useful feature
-            # (though it may be for Curve?)
+            # TODO not all Signals define self.duration, and DC should actually be implemented as a Transform here.
+            # TODO also, this product below is probably not even defined and should throw an error.
             other = other*DC(duration=self.duration)
-        
-        s = Mix()
-        
+
+        signal = Mix()
+
         if not self.transforms and isinstance(self, Mix):
-            s.signals += self.signals
+            signal.signals += self.signals  # TODO shouldn't we copy each first?
         else:
-            s.signals += [self]
-        
+            signal.signals += [self]  # TODO likewise
+
         if not other.transforms and isinstance(other, Mix):
-            s.signals += other.signals
+            signal.signals += other.signals  # TODO likewise
         else:
-            s.signals += [other]
-        
-        return s
-    
+            signal.signals += [other]  # TODO likewise
+
+        return signal
+
     def _repeat(self, number):
-        # repeats this signal several times in a row
+        """
+        Returns a new Signal, which is the concatenation of the current Signal to itself as many times as specified.
+        """
         assert isinstance(number, int)
-        return Signal.concat(*[self]*number)
-    
-    def _print_nice(self):
+        return Signal.concat(*[self]*number)  # TODO should we copy here?
+
+    #### overloading operators
+    def __str__(self):
+        """
+        For debugging purposes, describes the Signal structrue recursively, naming the Signals and Transforms involved
+        in it. Square brackets indicate a Sequence (concatenation), and parentheses indicate a Mix.
+        """
         if isinstance(self, Sequence):
             res = "[{}]".format(" + ".join([str(signal) for signal in self.sequence]))
         elif isinstance(self, Mix):
             res = "({})".format(" + ".join([str(signal) for signal in self.signals]))
         else:
             res = str(type(self).__name__)
-        
-        res += "" if not self.transforms else "*({})".format(",".join([str(transform) for transform in self.transforms]))
+
+        res += "" if not self.transforms else "*({})".format(
+            ",".join([str(transform) for transform in self.transforms]))
         return res
-    
-    ####### overloading operators #######
-    
-    def __str__(self):
-        return self._print_nice()
-    
+
     def __pow__(self, other):
         return self._repeat(other)
-    
+
     def __rmul__(self, other):
-        """ multiplication from the left is only legal for numbers, not for transforms"""
+        """Multiplication from the left is only valid for numbers (amplitude), not for other transforms."""
+        # TODO potential confusion for transforms that are not linear, since this will always be applied first,
+        # which is not necessarily the desired behavior when left-multiplying with a number.
         return self._amplitude(other)
-    
+
     def __mul__(self, other):
-        """
-        signals can be multiplied on the right by both floats and transforms
-        """
         return self._apply(other)
-    
+
     def __radd__(self, other):
+        # implemented solely for sum()
         if other == 0:
             return self
-        
-        raise TypeError("Signal can only be added to other signals, or to 0.")
-    
+
+        raise TypeError("Signals can only be added to other Signals, or to 0.")
+
     def __add__(self, other):
-        """
-        create empty signal list.
-        for each operand, if it is transformed or a single signal,
-        append it to the list.
-        otherwise, it is an untransformed list, simply extend the signal list.
-        """
         return self._mix(other)
-    
+
     def __sub__(self, other):
         return self._mix(-other)
-    
+
     def __neg__(self):
         return self._amplitude(-1.0)
-    
+
     def __or__(self, other):
-        """Overloading concat."""
         return self._concat(other)
-    
+
     def __ror__(self, other):
+        # TODO this originated in symmetry to __radd__, but is probably not needed at all. Consider removing.
         if other == 0:
             return self
-        
+
         if isnumber(other):
             return Silence(duration=other)._concat(self)
-        
-        raise TypeError("Signal can only be concatted to other signals, or to 0.")
-    
-    #########
+
+        raise TypeError("Signals can only be concatted to other Signals, or to 0.")
+
+    #### indexing and surgical operations
     @staticmethod
     def __subscripts(arg):
         """ Accepts first arg from __get/setitem__ and expands it to include
@@ -290,7 +345,7 @@ class Signal:
             transform it into an equivalent slice
         """
         assert isinstance(arg, (tuple, int, slice))
-        
+
         if isinstance(arg, tuple):
             assert len(arg) == 2, "improper amount of subscripts"
             assert isinstance(arg[1], slice), "time may only be described as ranges"
